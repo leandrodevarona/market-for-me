c0f354a5bda13fee5730f06fb538457eb3ffed8c
leandrodevarona <soporte_1@danngos.com>
2024-08-10 20:06:14 -0400

    Agregando logica de autenticación

---
+++ src/pages/api/signup.ts
@@ -0,0 +1,109 @@
+import { lucia } from "../../auth";
+import { generateId } from "lucia";
+import { hash } from "@node-rs/argon2";
+import { db } from "../../lib/db";
+
+import type { APIContext } from "astro";
+
+export async function POST(context: APIContext): Promise<Response> {
+  const formData = await context.request.formData();
+  const username = formData.get("username");
+  const email = formData.get("email");
+  if (
+    typeof email !== "string" ||
+    email.length < 3 ||
+    email.length > 100 ||
+    !/^[a-z0-9_-]+$/.test(email)
+  ) {
+    return new Response(
+      JSON.stringify({
+        error: "Invalid email",
+      }),
+      {
+        status: 400,
+      }
+    );
+  }
+  // username must be between 4 ~ 31 characters, and only consists of lowercase letters, 0-9, -, and _
+  // keep in mind some database (e.g. mysql) are case insensitive
+  if (
+    typeof username !== "string" ||
+    username.length < 3 ||
+    username.length > 31 ||
+    !/^[a-z0-9_-]+$/.test(username)
+  ) {
+    return new Response(
+      JSON.stringify({
+        error: "Invalid username",
+      }),
+      {
+        status: 400,
+      }
+    );
+  }
+  const password = formData.get("password");
+  if (
+    typeof password !== "string" ||
+    password.length < 6 ||
+    password.length > 255
+  ) {
+    return new Response(
+      JSON.stringify({
+        error: "Invalid password",
+      }),
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const passwordHash = await hash(password, {
+    // recommended minimum parameters
+    memoryCost: 19456,
+    timeCost: 2,
+    outputLen: 32,
+    parallelism: 1,
+  });
+  const userId = generateId(15);
+
+  try {
+    await db.user.create({
+      data: {
+        id: userId,
+        name: username,
+        email,
+        username,
+        passwordHash,
+      },
+    });
+
+    const session = await lucia.createSession(userId, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    context.cookies.set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes
+    );
+
+    return new Response();
+  } catch (e) {
+    if (e) {
+      return new Response(
+        JSON.stringify({
+          error: "Username already used",
+        }),
+        {
+          status: 400,
+        }
+      );
+    }
+    return new Response(
+      JSON.stringify({
+        error: "An unknown error occurred",
+      }),
+      {
+        status: 500,
+      }
+    );
+  }
+}
