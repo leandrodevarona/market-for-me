c8de6c31ef5b00ba8b7540cf9aefa137d24110dc
leandrodevarona <soporte_1@danngos.com>
2024-08-13 17:02:29 -0400

    Añadiendo libreria de iconos

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
 import auth from "auth-astro";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
   scopedStyleStrategy: "class",
   experimental: {
-    serverIslands: true,
+    serverIslands: true
   },
-  integrations: [auth()],
+  integrations: [auth(), icon()]
 });
